d6f1ceeeec523edcb236abe5c8f0d647237fab48
MikleIDK <98767348+MikleIDK@users.noreply.github.com>
2024-11-20 09:25:52 +0100

    ju

--- Levels/Enemy.gd
+++ Levels/Enemy.gd
@@ -1,8 +1,8 @@
 extends Node3D
 
+
 var front_ray
 var back_ray
-var animation
 
 const TRAVEL_TIME = 0.5
 var tween
@@ -10,10 +10,8 @@ var tween
 func _ready():
 	front_ray = $FrontRay
 	back_ray = $BackRay
-	animation = $Animation
 
-func addStep():
-	G.step += 1
+
 
 func _physics_process(_delta):
 	if tween != null and tween.is_running():
@@ -22,27 +20,22 @@ func _physics_process(_delta):
 	if Input.is_action_pressed("forward") and not front_ray.is_colliding():
 		tween = create_tween().set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
 		tween.tween_property(self, "transform", transform.translated_local(Vector3.FORWARD * 2), TRAVEL_TIME)
-		animation.play("HeadBob")
 		
-		addStep()
+	
 
 	if Input.is_action_pressed("back") and not back_ray.is_colliding():
 		tween = create_tween().set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
 		tween.tween_property(self, "transform", transform.translated_local(Vector3.BACK * 2), TRAVEL_TIME)
-		animation.play("HeadBob")
 		
-		addStep()
+
 
 	if Input.is_action_pressed("left"):
 		tween = create_tween().set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
 		tween.tween_property(self, "transform:basis", transform.basis.rotated(Vector3.UP, PI / 2), TRAVEL_TIME)
-		animation.play("HeadTurnLeft")
 		
-		addStep()
+
 
 	if Input.is_action_pressed("right"):
 		tween = create_tween().set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
 		tween.tween_property(self, "transform:basis", transform.basis.rotated(Vector3.UP, -PI / 2), TRAVEL_TIME)
-		animation.play("HeadTurnRight")
 		
-		addStep()
