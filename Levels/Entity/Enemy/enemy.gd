da0360bce5441da17d99ab3d101fcc339db5b17c
MKH05 <mattias@haugvik.dk>
2024-11-20 09:35:57 +0100

    FixedFinal

---
+++ Levels/Entity/Enemy/enemy.gd
@@ -0,0 +1,34 @@
+extends CharacterBody3D
+
+var front_ray
+var back_ray
+
+const TRAVEL_TIME = 0.5
+var tween
+
+func _ready():
+	front_ray = $FrontRay
+	back_ray = $BackRay
+
+func _physics_process(_delta):
+	if tween != null and tween.is_running():
+		return
+
+	if Input.is_action_pressed("forward") and not front_ray.is_colliding():
+		tween = create_tween().set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
+		tween.tween_property(self, "transform", transform.translated_local(Vector3.FORWARD * 2), TRAVEL_TIME)
+
+
+	if Input.is_action_pressed("back") and not back_ray.is_colliding():
+		tween = create_tween().set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
+		tween.tween_property(self, "transform", transform.translated_local(Vector3.BACK * 2), TRAVEL_TIME)
+
+
+	if Input.is_action_pressed("left"):
+		tween = create_tween().set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
+		tween.tween_property(self, "transform:basis", transform.basis.rotated(Vector3.UP, PI / 2), TRAVEL_TIME)
+
+
+	if Input.is_action_pressed("right"):
+		tween = create_tween().set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
+		tween.tween_property(self, "transform:basis", transform.basis.rotated(Vector3.UP, -PI / 2), TRAVEL_TIME)
