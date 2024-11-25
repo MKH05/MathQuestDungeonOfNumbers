0d405bd707b51099b47a1629db06828760e40784
MKH05 <mattias@haugvik.dk>
2024-11-25 13:13:35 +0100

    AddedMinimap

--- Scripts/gui.gd
+++ Scripts/gui.gd
@@ -1,7 +1,17 @@
 extends Control
 
+@export var target : NodePath
+@export var camera_distance := 10.0
+
+@onready var player := get_node(target)
+@onready var camera := $Map/SubViewport/Camera3D
+
 func _ready() -> void:
 	$StepLabel.text = "Step: " + str(G.step)
 
 func _process(delta: float) -> void:
 	$StepLabel.text = "Step: " + str(G.step)
+	
+	if target:
+		camera.size = camera_distance
+		camera.position = Vector3(player.position.x, camera_distance, player.position.z)
