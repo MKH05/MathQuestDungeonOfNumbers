3f32cfd266d424dfe89ab944e3cca687cbe87c35
MKH05 <mattias@haugvik.dk>
2024-11-27 08:56:27 +0100

    Added player movement cooldown

--- Assets/Environment/Levels/Entity/Player/player.gd
+++ Assets/Environment/Levels/Entity/Player/player.gd
@@ -6,45 +6,50 @@ var detect_ray
 var animation
 
 const TRAVEL_TIME = 0.5
+const COOLDOWN_TIME = 1.5
+
 var tween
+var cooldown_timer
 
 func _ready():
 	front_ray = $FrontRay
 	back_ray = $BackRay
 	detect_ray = $DetectRay
 	animation = $Animation
+	cooldown_timer = $cooldown_timer
 
 func addStep():
 	G.step += 1
+	cooldown_timer.start()
 
 func _physics_process(_delta):
 	if tween != null and tween.is_running():
 		return
-
-	if Input.is_action_pressed("forward") and not front_ray.is_colliding():
+		
+	if Input.is_action_pressed("forward") and not front_ray.is_colliding() and cooldown_timer.is_stopped():
 		tween = create_tween().set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
 		tween.tween_property(self, "transform", transform.translated_local(Vector3.FORWARD * 2), TRAVEL_TIME)
 		animation.play("HeadBob")
-		
+			
 		addStep()
 
-	if Input.is_action_pressed("back") and not back_ray.is_colliding():
+	elif Input.is_action_pressed("back") and not back_ray.is_colliding() and cooldown_timer.is_stopped():
 		tween = create_tween().set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
 		tween.tween_property(self, "transform", transform.translated_local(Vector3.BACK * 2), TRAVEL_TIME)
 		animation.play("HeadBob")
-		
+			
 		addStep()
 
-	if Input.is_action_pressed("left"):
+	elif Input.is_action_pressed("left") and cooldown_timer.is_stopped():
 		tween = create_tween().set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
 		tween.tween_property(self, "transform:basis", transform.basis.rotated(Vector3.UP, PI / 2), TRAVEL_TIME)
 		animation.play("HeadTurnLeft")
-		
+			
 		addStep()
 
-	if Input.is_action_pressed("right"):
+	elif Input.is_action_pressed("right") and cooldown_timer.is_stopped():
 		tween = create_tween().set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
 		tween.tween_property(self, "transform:basis", transform.basis.rotated(Vector3.UP, -PI / 2), TRAVEL_TIME)
 		animation.play("HeadTurnRight")
-		
+			
 		addStep()
